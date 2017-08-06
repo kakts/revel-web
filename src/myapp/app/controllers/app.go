ae24d596667aecc12b4e881aa274effa4f89f5e6
kakts <kakts.git@gmail.com>
2017-08-07 02:11:03 +0900

    [WIP] Add Login Controller

--- src/myapp/app/controllers/app.go
+++ src/myapp/app/controllers/app.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"log"
+
 	"github.com/revel/revel"
 )
 
@@ -24,4 +26,44 @@ func (c App) Hello(myName string) revel.Result {
 	}
 
 	return c.Render(myName)
+}
+
+// GET:/login
+// TODO
+func (c App) Login(id string, pass string) revel.Result {
+	log.Println("test")
+	if c.Validation.HasErrors() {
+		log.Println("error")
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+
+	// success redirect to mypage
+	// set cookie
+	// TODO
+	return c.Render()
+}
+
+// POST:/login
+// TODO
+func (c App) DoLogin(id string, pass string) revel.Result {
+	log.Println("POST:/login")
+	// validation for id
+	c.Validation.Required(id).Message("id is required")
+	c.Validation.MinSize(id, 8).Message("Id should be over 8 characters")
+
+	// validation for id
+	c.Validation.Required(pass).Message("pass is required")
+	c.Validation.MinSize(pass, 8).Message("pass should be over 8 characters")
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Login)
+	}
+
+	// success redirect to mypage
+	// set cookie
+	// TODO
+	return c.Render()
 }
