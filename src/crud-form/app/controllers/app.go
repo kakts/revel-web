7bd616c98238eb1981a5665afc7bd68da3dedc46
Keita Akutsu <kakts.git@gmail.com>
2017-09-05 01:08:37 +0900

    [WIP]Add register form

--- src/crud-form/app/controllers/app.go
+++ src/crud-form/app/controllers/app.go
@@ -4,6 +4,9 @@ import (
 	"github.com/revel/revel"
 
     "log"
+
+    "crud-form/app/models"
+    "crud-form/app/routes"
 )
 
 type App struct {
@@ -25,6 +28,26 @@ func (c App) Test() revel.Result {
 	return c.Render()
 }
 
-func init() {
+func (c App) Register() revel.Result {
+    return c.Render()
+}
 
+func (c App) SaveUser(user models.User, verifyPassword string) revel.Result {
+    c.Validation.Required(verifyPassword)
+    c.Validation.Required(verifyPassword == user.Password).Message("Password does not match")
+    user.Validate(c.Validation)
+
+    // Error時の処理
+    if c.Validation.HasErrors() {
+        c.Validation.Keep()
+        c.FlashParams()
+        return c.Render(routes.App.Index())
+    }
+
+
+    c.Session["user"] = user.Username
+    c.Flash.Success("Welcome, " + user.Name)
+
+    // TODO routerがおかしい
+    return c.Render(routes.App.Index())
 }
--- src/crud-form/app/models/user.go
+++ src/crud-form/app/models/user.go
@@ -8,10 +8,37 @@ import (
 
 // Simple User Class
 type User struct {
-    UserId int
-    Name string
+    UserId      int
+    Name        string
+    Username, Password string
+    HashedPassword     []byte
 }
 
 func (u *User) String() string {
     return fmt.Sprintf("User(%s)", u.Name)
+}
+
+var userRegex = regexp.MustCompile("^\\w*$")
+
+func (user *User) Validate(v *revel.Validation) {
+    v.Check(user.Name,
+        revel.Required{},
+        revel.MaxSize{15},
+        revel.MinSize{4},
+    )
+
+    ValidatePassword(v, user.Password).Key("user.Password")
+
+    v.Check(user.Name,
+        revel.Required{},
+        revel.MaxSize{100},
+    )
+}
+
+func ValidatePassword(v *revel.Validation, password string) *revel.ValidationResult {
+    return v.Check(password,
+        revel.Required{},
+        revel.MaxSize{15},
+        revel.MinSize{5},
+    )
 }
